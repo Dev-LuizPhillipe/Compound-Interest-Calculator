a44d5f95461d9b75950612d87eff7831b8263600
Dev-LuizPhillipe <lord.luiz2014@gmail.com>
2025-02-04 14:50:05 -0300

    Preparing the environment

---
+++ src/themes/themes.tsx
@@ -0,0 +1,10 @@
+//Collors for the app
+const themes = {
+  collors: {
+    primary: "#040f0fff",
+    secondary: "#2d3a3aff",
+    background: "#d9dcd6ff",
+    primarygreen: "#32e875ff",
+    secondarygreen: "#33a075ff",
+  },
+};
