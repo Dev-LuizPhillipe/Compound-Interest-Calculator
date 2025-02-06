ff464bb0738e48f979959f74c2bd9c856585d27d
Dev-LuizPhillipe <lord.luiz2014@gmail.com>
2025-02-06 03:08:30 -0300

    working on the formula and operation of inputs and state management

--- src/components/InputBox/InputBox.tsx
+++ src/components/InputBox/InputBox.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./InputBox.css";
 
-function InputBox() {
+function InputBox({ icon, placeholder, value, onChange }) {
   return (
     <div className="input-box">
       <div className="input-box-side">
-        <h1 className="input-icon">R$</h1>
+        <h1 className="input-icon">{icon}</h1>
       </div>
       <form className="form-box">
         <input
           type="number"
           className="input-form"
-          placeholder="Digite o valor inical"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
         />
       </form>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/ButtonPage/button";
 import Header from "../components/Header/header";
 import InputBox from "../components/InputBox/InputBox";
 
-export default function Home() {
+function Home() {
+  //state to store the values of the inputs
+  const [state, setState] = useState({
+    initialValue: 0,
+    initialInvestment: 0,
+    interestRate: 0,
+    periodMonths: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: Number(value),
+    });
+  };
+
+  const handleCalculateInterest = (
+    initialValue: number,
+    monthlyInvestment: number,
+    interestRate: number,
+    periodMonths: number
+  ): number => {
+    const result =
+      initialValue * 1 +
+      interestRate / 10 ** periodMonths +
+      monthlyInvestment *
+        ((1 + interestRate ** periodMonths - 1) / interestRate);
+
+    console.log(result);
+    return result;
+  };
+
   return (
     <div className="containerHome">
       <Header />
       <div className="contentHome">
         <div>
           <h1>Valor Inicial:</h1>
-          <InputBox />
+          <InputBox
+            icon={"R$"}
+            placeholder="Digite o valor inicial"
+            value={state.initialValue}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <h1>Investimento mensal:</h1>
-          <InputBox />
+          <InputBox
+            icon={"R$"}
+            placeholder="Digite o investimento"
+            value={state.initialInvestment}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <h1>Taxa de juros:</h1>
-          <InputBox />
+          <InputBox
+            icon={"%"}
+            placeholder="Digite a taxa de juros"
+            value={state.interestRate}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <h1>Período:</h1>
-          <InputBox />
+          <InputBox
+            icon={"#"}
+            placeholder="Digite o período"
+            value={state.periodMonths}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
+      W
       <div>
         <Button
           color={"#040f0f"}
           backgroundColor={"#32e875"}
-          onClick={() => console.log("Calculando...")}
+          onClick={handleCalculateInterest}
         />
+        <p>Valor total investido: {state.initialValue}</p>
+        <p>valor total acumulado: {state.initialValue}</p>
+        <p>valor total taxas: {state.initialValue}</p>
       </div>
     </div>
   );
 }
+
+export default Home;
